a251c8b61d1efc3e41ccd578502d00957162a67f
Pablo Fajardo <pablofajardoruano@gmail.com>
2023-05-20 21:20:38 -0500

    Add tailwind and mobile-version

--- app/controllers/favorites_controller.rb
+++ app/controllers/favorites_controller.rb
@@ -1,7 +1,29 @@
 class FavoritesController < ApplicationController
+  
+  def index
+    
+  end
   def create
-    Favorite.create(product: product, user: Current.user)
-    redirect_to product_path(product)
+    product.favorite!
+    respond_to do |format|
+      format.html do
+        redirect_to product_path(product)
+      end
+      format.turbo_stream do
+      render turbo_stream: turbo_stream.replace("favorite",partial:"products/favorite",locals:{product:product})
+      end
+    end
+  end
+  def destroy
+    product.unfavorite!
+    respond_to do |format|
+      format.html do
+        redirect_to product_path(product), status: :see_other
+      end
+      format.turbo_stream do
+        render turbo_stream: turbo_stream.replace("favorite",partial:"products/favorite",locals:{product:product})
+      end
+    end
   end
 
   private
